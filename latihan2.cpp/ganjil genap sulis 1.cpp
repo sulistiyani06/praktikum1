b65909e055dbba9f6eb0546b8bc84dffbaf86335
sulistiyani <sulistiyani@mhs.pelitabangsa.ac.id>
2018-10-09 14:36:59 +0700

    tambah latihan2.cpp : program ganjil genap sulis 1

---
+++ latihan2.cpp/ganjil genap sulis 1.cpp
@@ -0,0 +1,25 @@
+ #include<iostream>
+ using namespace std;
+
+    int main () {
+
+    int angka, hasil;
+
+    cout << " masukan bilangan: ";
+    cin >> angka;
+    cout << endl;
+
+    hasil=angka % 2;
+
+    if(hasil== 0) {
+
+        cout << angka << "sama dengan bilangan genap" << endl;
+
+
+    }else{
+            cout << " sama dengan bilangan ganjil " <<endl;
+
+    }
+
+    }
+
