665cb2b38430aa49d7123804c1d17154b2c23c93
sulistiyani <sulistiyani@mhs.pelitabangsa.ac.id>
2018-10-09 14:35:27 +0700

    tambah latihan1.cpp: program lpp sulis

---
+++ latihan1.cpp/lpp sulis.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+    int main(){
+      int panjang,lebar,luas;
+
+      panjang=5;
+      lebar=5;
+
+      luas=panjang * lebar;
+
+      cout << "panjang: " << panjang << endl;
+      cout << "lebar   : " << lebar ;
+      cout << endl;
+      cout << "rumus mencari luas : " << panjang << " x " << lebar;
+      cout << endl;
+      cout << " luas: " << luas;
+      cout << endl;
+
+    }
